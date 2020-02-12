dfd28813ac4b96c275179a244591889bd5cce13a
Matt Callanan <mattcallanan@users.noreply.github.com>
2020-02-12 16:01:11 +1000

    metrics-functions: Give Graphite time to receive all of the metrics for the last bin

--- metrics/src/main/java/com/expedia/adaptivealerting/metrics/functions/source/graphite/ConstructSourceURI.java
+++ metrics/src/main/java/com/expedia/adaptivealerting/metrics/functions/source/graphite/ConstructSourceURI.java
@@ -22,40 +22,13 @@ import lombok.val;
 import static com.expedia.adaptivealerting.anomdetect.util.ThreadUtil.sleep;
 
 public class ConstructSourceURI {
-    public static final int ONE_MINUTE = 60;
     private final String GRAPHITE_URI_KEY = "urlTemplate";
 
     public String getGraphiteURI(Config metricSourceSinkConfig, MetricFunctionsSpec metricFunctionsSpec) {
-        String graphiteUri = metricSourceSinkConfig.getString(GRAPHITE_URI_KEY);
-        val intervalInSecs = metricFunctionsSpec.getIntervalInSecs();
-        val currentGraphiteEpochSeconds = System.currentTimeMillis() / 1000;
-        val startOfCurrentInterval = (currentGraphiteEpochSeconds / intervalInSecs) * intervalInSecs;
-        val startOfPreviousInterval = startOfCurrentInterval - intervalInSecs;
-        sleepUntilPreviousIntervalIsFull(intervalInSecs, currentGraphiteEpochSeconds, startOfCurrentInterval);
-        return String.format("%s%s&from=%d&until=%d",
-                graphiteUri,
-                metricFunctionsSpec.getFunction(),
-                startOfPreviousInterval,
-                startOfCurrentInterval-1);
-    }
-
-    /**
-     * We need to give Graphite time to receive all of the metrics for the last interval (where interval equals passage of time intervalInSecs long).
-     * We assume that Graphite is using round numbers for intervals.
-     * We wait until enough time has passed since last interval.  Time is considered enough when at least one minute or half of intervalInSecs
-     * (whichever is smaller) has passed since start of current interval.
-     * @param intervalInSecs
-     * @param currentGraphiteEpochSeconds
-     * @param startOfCurrentInterval
-     */
-    private void sleepUntilPreviousIntervalIsFull(int intervalInSecs, long currentGraphiteEpochSeconds, long startOfCurrentInterval) {
-        val secondsSinceCurrentIntervalStarted = currentGraphiteEpochSeconds - startOfCurrentInterval;
-        val halfIntervalInSecs = intervalInSecs / 2;
-        val secondsUntilPreviousIntervalIsFull = Math.min(ONE_MINUTE, halfIntervalInSecs - secondsSinceCurrentIntervalStarted);
-        boolean tooSoonSinceLastInterval = secondsSinceCurrentIntervalStarted < secondsUntilPreviousIntervalIsFull;
-        if (tooSoonSinceLastInterval) {
-            sleep(secondsUntilPreviousIntervalIsFull * 1000);
-        }
+        return metricSourceSinkConfig.getString(GRAPHITE_URI_KEY)
+                + metricFunctionsSpec.getFunction()
+                + GRAPHITE_FROM_TIME_PARAM_STRING
+                + metricFunctionsSpec.getIntervalInSecs() + GRAPHITE_TIME_UNIT_STRING;
     }
 
 }
